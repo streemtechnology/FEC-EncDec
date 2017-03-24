27b26a710c6f7e579db429a1e4128d7e3e85ee3a
root <root@streem-ubuntu-ldn.localdomain>
2017-03-24 00:25:08 +0000

    Problem with serializing object cpickle error

--- rtmp2fec_sender.py
+++ rtmp2fec_sender.py
@@ -2,6 +2,7 @@ import os, sys
 from socket import socket, AF_INET, SOCK_DGRAM
 import librtmp
 from pylonghair import fec_encode, fec_decode
+import cPickle
 
 def get_stream():
     #conn = librtmp.RTMP("rtmp://188.138.17.8:1935/albuk/albuk.stream", live=True)
@@ -11,10 +12,9 @@ def get_stream():
     rtmpstream = conn.create_stream()
     sock = socket(AF_INET,SOCK_DGRAM)
     sock.connect(('127.0.0.1',5006))# stream data
-    get_bytes(rtmpstream,sock,sock1)
+    get_bytes(rtmpstream,sock)
 
 def get_bytes(stream,sock):
-    frame =0
     while True:
         data = stream.read(8192)
         #rtmpwritestream.write(data) # This works perfectly
@@ -22,30 +22,29 @@ def get_bytes(stream,sock):
             if len(data) > 8192:
                 print "Cant handle more than 8192 bytes right now, Got bytes: ", len(data)
                 sys.exit(0)
-            encode_to_fec(data,sock,frame%8)
-            frame+=1
+            encode_to_fec(data,sock)
         else:
             print "Waiting for Data on the Stream"
             sys.exit(0)
 
-def encode_to_fec(data,sock,frame):
+def encode_to_fec(data,sock):
     block_size = 512
     k = 16   # 512*16 = 8192 send all blocks of received data
     m = 4
     parity = bytearray(m * block_size)
     encoded_data = fec_encode(k, m, block_size, data, parity)
     if encoded_data == 0:
-        print "Encoded to FEC Data Bytes: "#, len(data), "Data Content:",  data
+        #print "Encoded to FEC Data Bytes: "#, len(data), "Data Content:",  data
         blocks = []
         for row in range(k):
             offset = row * block_size
             block_data = data[offset:offset + block_size]
-            blocks.append((frame, row, block_data))
+            blocks.append(( row, block_data))
         send_fec_data( parity, blocks,sock)
 
 def send_fec_data(parity,blocks,sock):
    for block in blocks:
-       sock1.send(str(block))
+       sock.send(cPickle.dumps(block))
    #sock.send(parity)
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -2,41 +2,68 @@ import socket
 import os, sys
 import librtmp
 from pylonghair import fec_encode, fec_decode
+import cPickle
+
+recv_count = 0
+block_data = bytearray(16*512)
 
 def receive_write_stream():
     UDP_IP = "127.0.0.1"
-    UDP_PORT = 5005
     reconstruct_rtmp = librtmp.RTMP("rtmp://178.62.61.235:1935/show/stream_name", live=True)
     reconstruct_rtmp.connect()
     rtmpwritestream = reconstruct_rtmp.create_stream(True,True)
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
-    sock1 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
-    sock.bind((UDP_IP, UDP_PORT))
-    sock1.bind((UDP_IP, 5006))
-    get_bytes(sock1,rtmpwritestream)
+    sock.bind((UDP_IP, 5006))
+    get_bytes(sock,rtmpwritestream)
 
-def get_bytes(sock1,stream):
+def get_bytes(sock,stream):
     while True:
-        data = sock1.recvfrom(512)
+        data,addr = sock.recvfrom(512)
         #rtmpwritestream.write(data) # This works perfectly
         if data:
             if len(data) > 512:
                 print "Cant handle more than 8192 bytes right now, Got bytes: ", len(data)
                 sys.exit(0)
             #decode_from_fec(data,stream)
-            data = tuple(data[0].split(','))
-            print data[0] , "   " , data[1]
+	    print cPickle.loads(data)
+            #data = tuple(data[0].split(','))
+            decode_from_fec(data[0].split('(')[1],data[1],stream)
         else:
             print "Waiting for Data on the Stream"
             sys.exit(0)
 
-def decode_from_fec(blocks,stream):
+def decode_from_fec(row,data,stream):
+    global recv_count
+    global block_data
+    block_size =512
     k = 16
-    m = 4
-    block_size = 512
-    decoded_data = fec_decode(k, m, block_size, blocks)
-    if decoded_data == 0:
-        construct_rtmp(blocks,stream)
+    offset  = int(row)*block_size
+    block_data[offset:offset+block_size] = data
+    recv_count+=1
+    #if recv_count ==0:
+    #    block_data[offset:offset+block_size] = data
+    #	recv_count+=1
+    #else:
+        # havent seen this block before
+    #    block_data[offset:offset+block_size] = data
+    #    recv_count+=1
+        #if block_data[row]!=0:
+	#    block_data[row:row+block_size] = data
+	#    recv_count+=1
+    # if received all blocks
+    if recv_count == 16:
+        recv_count =0
+        try:
+            stream.write(block_data)   
+        except IOError:
+            print "error" 
+                        
+ 
+    
+    # decode using below for row > k and check for frame 
+    #decoded_data = fec_decode(k, m, block_size, blocks)
+    #if decoded_data == 0:
+    #    construct_rtmp(blocks,stream)
 
 
 def construct_rtmp(blocks,stream):
@@ -46,7 +73,7 @@ def construct_rtmp(blocks,stream):
     data = bytearray()
     for block in blocks:
         #print blocks, block
-	data+=block[1]
+        data+=block[1]
         #rtmpwritestream.write(block[1])
         #print rtmpwritestream.write(block[0])
     stream.write(data)
